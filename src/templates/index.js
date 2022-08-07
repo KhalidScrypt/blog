a63eb7942ddd8c739d357d0fc849ebfcc2231cde
Khalid Belghiti <33843956+KhalidScrypt@users.noreply.github.com>
2022-08-07 09:40:48 +0200

    correct sendinblue

--- src/templates/index.js
+++ src/templates/index.js
@@ -113,19 +113,6 @@ const IndexPage = ({ data, pageContext }) => {
                 )}
                 <Pagination page={pageContext.currentPage} numOfPages={pageContext.numPages} />
             </Container>
-<script>
-  window.REQUIRED_CODE_ERROR_MESSAGE = 'Please choose a country code';
-  window.LOCALE = 'en';
-  window.EMAIL_INVALID_MESSAGE = window.SMS_INVALID_MESSAGE = "The information provided is invalid. Please review the field format and try again.";
-
-  window.REQUIRED_ERROR_MESSAGE = "This field cannot be left blank. ";
-
-  window.GENERIC_INVALID_MESSAGE = "The information provided is invalid. Please review the field format and try again.";
-
-
-  var AUTOHIDE = Boolean(0);
-</script>
-<script src="https://sibforms.com/forms/end-form/build/main.js"></script>
         </Layout>
         
     );
